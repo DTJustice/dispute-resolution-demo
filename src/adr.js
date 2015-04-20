ac57e4b68fde1b00c715c844ae16bd17179a81df
Ben Boyle <benjamins.boyle@gmail.com>
2015-04-21 09:22:17 +1000

    load data from JSONP
    - read JSONP data into arrays for dispute types, levels and parties
    - populate datalist options from values in source data

--- src/adr.js
+++ src/adr.js
@@ -1,5 +1,51 @@
+// JSONP receiver
+var getAlternativeDisputeResolutionData;
+
+
 (function() {
 	'use strict';
+
+
+	var data, disputeLevel = [], partyType = [], disputeType = [];
+
+	getAlternativeDisputeResolutionData = function( d ) {
+		function createOptionsFromArray( item ) {
+			var span = document.createElement( 'span' );
+			span.innerText = item; // escape HTML
+			return '<option>' + span.innerHTML + '</option>';
+		}
+
+		data = d;
+
+		for ( var i = 0, len = d.length; i < len; i++ ) {
+			// ES6: let, exploder
+			var level = d[ i ].Level.split( /\s*;\s*/ );
+			var party = d[ i ].Party.split( /\s*;\s*/ );
+			var type = d[ i ].Dispute.split( /\s*;\s*/ );
+
+			disputeLevel = disputeLevel.concat( level.filter(function( item ) {
+				return disputeLevel.indexOf( item ) === -1;
+			}));
+			partyType = partyType.concat( party.filter(function( item ) {
+				return partyType.indexOf( item ) === -1;
+			}));
+			disputeType = disputeType.concat( type.filter(function( item ) {
+				return disputeType.indexOf( item ) === -1;
+			}));
+		}
+
+		disputeLevel.sort();
+		partyType.sort();
+		disputeType.sort();
+
+		// create option elements from arrays
+		// easy to do with angular scope variable and ng-repeat
+		// what about backbone? angular2?
+		document.getElementById( 'dispute-level-list' ).innerHTML = disputeLevel.map( createOptionsFromArray ).join( '' );
+		document.getElementById( 'party-type-list' ).innerHTML = partyType.map( createOptionsFromArray ).join( '' );
+		document.getElementById( 'dispute-type-list' ).innerHTML = disputeType.map( createOptionsFromArray ).join( '' );
+	};
+
 
 
 	function calculateWidth( event ) {
