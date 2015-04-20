c156bd3e732c4b2797df2ca209eee4436b0442a9
Ben Boyle <benjamins.boyle@gmail.com>
2015-04-21 09:07:30 +1000

    add sample JSONP data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+// JSONP data
+getAlternativeDisputeResolutionData([{
+	Comments: 'Code of conduct',
+	URL: 'http://www.psc.qld.gov.au/includes/assets/qpd-code-conduct.pdf',
+	Level: 'concern; problem; disagreement; complaint; conflict; dispute',
+	Party: 'someone I supervise; my supervisor; someone on a similar level; a co-worker; a team member; a peer; my manager',
+	Dispute: 'bullying; harassment; unfair treatment; discrimination; criticism; gossip; health and wellbeing; performance management',
+	Resolution: 'Self; Assisted; Formal'
+}, {
+	Comments: 'Sexual harassment',
+	URL: 'http://www.adcq.qld.gov.au/complaints/sexual-harassment',
+	Level: 'concern; problem; complaint',
+	Party: 'someone I supervise; my supervisor; someone on a similar level; a co-worker; a team member; a peer; my manager',
+	Dispute: 'harassment',
+	Resolution: 'Self; Assisted; Formal'
+}, {
+	Comments: '-',
+	URL: 'http://intranet.justice.govnet.qld.gov.au/__data/assets/word_doc/0008/89648/employee-performance-management-guidelines.DOC',
+	Level: 'concern; problem',
+	Party: 'someone I supervise; my supervisor; my manager',
+	Dispute: 'performance management',
+	Resolution: 'Self; Assisted'
+}, {
+	Comments: '-',
+	URL: 'https://www.business.qld.gov.au/business/employing/staff-development/staff-performance-reviews',
+	Level: 'concern',
+	Party: 'someone I supervise',
+	Dispute: 'performance management',
+	Resolution: 'Self'
+}]);
