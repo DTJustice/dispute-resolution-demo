686f4f7f671209bba479ca9325996393b3a6698c
Ben Boyle <benjamins.boyle@gmail.com>
2015-05-04 10:51:02 +1000

    reorder resolution options based on dispute type
    - selecting 'dispute' will move self resolution group to end of results

---
+++ src/reoder-angular-adr.js
@@ -0,0 +1,58 @@
+// JSONP receiver
+var cachedData = [];
+var getAlternativeDisputeResolutionData = function( data ) {
+	cachedData = data;
+};
+
+
+// basic angular
+angular.module( 'disputeResolution', [] )
+
+.controller( 'resultsController', function( $scope ) {
+	'use strict';
+
+	$scope.results = cachedData;
+	$scope.resolutions = [ 'Self', 'Assisted', 'Formal' ];
+
+	$scope.model = {
+		disputeType: [],
+		party: [],
+		disputeSubject: []
+	};
+
+	for ( var i = 0, len = cachedData.length; i < len; i++ ) {
+		// ES6: let, exploder
+		var type = cachedData[ i ].disputeType.split( /\s*;\s*/ );
+		var party = cachedData[ i ].party.split( /\s*;\s*/ );
+		var subject = cachedData[ i ].disputeSubject.split( /\s*;\s*/ );
+
+		$scope.model.disputeType = $scope.model.disputeType.concat( type.filter(function( item ) {
+			return item.length && item !== 'undefined' && $scope.model.disputeType.indexOf( item ) === -1;
+		}));
+		$scope.model.party = $scope.model.party.concat( party.filter(function( item ) {
+			return item.length && item !== 'undefined' && $scope.model.party.indexOf( item ) === -1;
+		}));
+		$scope.model.disputeSubject = $scope.model.disputeSubject.concat( subject.filter(function( item ) {
+			return item.length && item !== 'undefined' && $scope.model.disputeSubject.indexOf( item ) === -1;
+		}));
+	}
+
+	$scope.story = {
+		disputeSubject: $scope.model.disputeSubject[ 0 ],
+		disputeType: $scope.model.disputeType[ 0 ],
+		party: $scope.model.party[ 0 ]
+	};
+
+
+	// change order of resolutions for disputes
+	$scope.$watch( 'story.disputeType', function() {
+		switch ( $scope.story.disputeType ) {
+		case 'Dispute':
+			$scope.resolutions = [ 'Assisted', 'Formal', 'Self' ];
+			break;
+		default:
+			$scope.resolutions = [ 'Self', 'Assisted', 'Formal' ];
+		}
+	});
+
+});
