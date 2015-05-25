81e5efcd8d8850c7c7c672a3df7475b1f945e425
Ben Boyle <benjamins.boyle@gmail.com>
2015-05-25 19:01:41 +1000

    initial page state and view routing
    - read state from URL to determine view
    - setup in SWE template
    - setup script path
    - setup template location

---
+++ src/dispute-pathways.js
@@ -0,0 +1,16 @@
+(function( $ ) {
+	'use strict';
+
+	// current page url
+	var story = $.url().param();
+
+	story.complete = story.have && story.with && story.about;
+
+	if ( story.complete ) {
+		$( '#dispute-pathways-view' ).text( 'RESULTS' );
+	} else {
+		$( '#dispute-pathways-view' ).text( 'FORM' );
+	}
+
+
+}( jQuery ));
